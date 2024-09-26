c58d62ea74d84dd5c9d6d71333adb8db5b5a59fe
Pranay Raut <pranayraut129@gmail.com>
2024-09-26 13:34:59 +0530

    Add project files.

---
+++ FactorialNumber/Models/Factorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber.Models
+{
+    internal class Factorial
+    {
+        //Calculating factorial of a number by linear method
+        public static double LinearFactorial(double number)
+        {
+            double result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+
+        //Calculating factorial of a number by recursive method
+        public static double RecursiveFactorial(double number)
+        {
+            if (number <= 0)
+            {
+                return 1;
+            }
+
+
+            return number * RecursiveFactorial(number - 1);
+        }
+    }
+}
---
+++ FactorialNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using FactorialNumber.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            double number = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"=====The factorial by liner method=====");
+            Console.WriteLine(Factorial.LinearFactorial(number));
+            Console.WriteLine();
+
+            Console.WriteLine($"=====The factorial by Recursive method=====");
+            Console.WriteLine(Factorial.RecursiveFactorial(number));
+        }
+    }
+}
